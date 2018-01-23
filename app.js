4bee0a549f62097fb0185d6678869b6496bd280d
MattWillcox <mattjwillcox@gmail.com>
2018-01-22 21:20:35 -0800

    mongodb config

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const app = express();
  * Connect to MongoDB.
  */
 mongoose.Promise = global.Promise;
-mongoose.connection.openUri(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connection.openUri(process.env.MONGOLAB_URI);
 mongoose.connection.on('error', (err) => {
   console.error(err);
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
