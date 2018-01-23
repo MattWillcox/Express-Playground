bd7c39742b0ae183b02b1211200a7b30429936a1
MattWillcox <mattjwillcox@gmail.com>
2018-01-22 21:32:38 -0800

    reverting urls

--- config/passport.js
+++ config/passport.js
@@ -453,8 +453,8 @@ passport.use('foursquare', new OAuth2Strategy({
 passport.use(new OpenIDStrategy({
   apiKey: process.env.STEAM_KEY,
   providerURL: 'http://steamcommunity.com/openid',
-  returnURL: 'https://agile-headland-24758.herokuapp.com/auth/steam/callback',
-  realm: 'https://agile-headland-24758.herokuapp.com/',
+  returnURL: 'http://localhost:3000/auth/steam/callback',
+  realm: 'http://localhost:3000/',
   stateless: true
 }, (identifier, done) => {
   const steamId = identifier.match(/\d+$/)[0];
